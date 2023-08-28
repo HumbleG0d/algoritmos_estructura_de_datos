fb1261c2b174686acb6371451235adf5bfba4fad
HumbleG0d <sergiouni2000@gmail.com>
2023-08-28 16:31:31 -0500

    add the main file where the methods of the linkedlist structure are used

---
+++ LinkedLists/Main.java
@@ -0,0 +1,24 @@
+package LinkedLists;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList();
+    System.out.println("Lista simple");
+    ll.insertFirst(1);
+    ll.insertFirst(2);
+    ll.insertFirst(4);
+    ll.insertFirst(5);
+    ll.insertLast(45);
+    ll.insert(1, 322);
+    ll.displayList();
+    System.out.println(ll.deleteFirst());
+    ll.displayList();
+    System.out.println(ll.deleteLast());
+    ll.displayList();
+    System.out.println(ll.delete(3));
+    ll.displayList();
+
+    System.out.println("Lista doble simple");
+
+  }
+}

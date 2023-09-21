e83d9d4d5dfae61e5aa60f85afedac978e5113c5
HumbleG0d <sergiouni2000@gmail.com>
2023-09-21 12:56:12 -0500

    Add the code of the DirectorioVr1 class that implements the Directory interface

---
+++ ProjectDirectorio/DirectorioVr1.java
@@ -0,0 +1,105 @@
+package ProjectDirectorio;
+
+public class DirectorioVr1 implements Directorio {
+
+  private static final int TAMANIO_PREDETERN = 25;
+  private static final int NO_ENCONTRADO = -1;
+
+  private Persona[] entrada;
+  private int contador;
+
+  public DirectorioVr1() {
+    this(TAMANIO_PREDETERN);
+  }
+
+  public DirectorioVr1(int tamanio) {
+    contador = 0;
+
+    if (tamanio <= 0) {
+      throw new IllegalArgumentException("El tamaño debe ser positivo");
+    }
+
+    entrada = new Persona[tamanio];
+    System.out.println("Se ha creado un arreglo de " + tamanio + " items.");
+  }
+
+  public Persona[] ordena(int atributo, int option) {
+    Persona[] listaOrdenada = new Persona[contador];
+
+    for (int i = 0; i < contador; i++) {
+      listaOrdenada[i] = entrada[i];
+    }
+
+    Persona.setComparaAtributo(atributo);
+
+    // * Comienza el ordenamiento */
+
+    listaOrdenada = ElegirSort.opcion(option, listaOrdenada, atributo, contador);
+
+    return listaOrdenada;
+  }
+
+  public void agrega(Persona nuevaPersona) {
+    if (contador == entrada.length) {
+      expande();
+    }
+    entrada[contador] = nuevaPersona;
+    contador++;
+  }
+
+  public boolean borra(String nombre) {
+    boolean estado;
+    int loc;
+
+    loc = encuentraIndice(nombre);
+
+    if (loc == NO_ENCONTRADO) {
+      estado = false;
+    } else {
+      entrada[loc] = entrada[contador - 1];
+      estado = true;
+      contador--;
+    }
+    return estado;
+  }
+
+  public Persona busca(String nombre) {
+    Persona encuentPersona;
+    int loc = 0;
+
+    while (loc < contador && (nombre.equals(entrada[loc].getNombre()))) {
+      loc++;
+    }
+
+    if (loc == contador) {
+      encuentPersona = null;
+    } else {
+      encuentPersona = entrada[loc];
+    }
+    return encuentPersona;
+  }
+
+  private void expande() {
+    int nuevaLongitud = (int) (1.5 * entrada.length);
+    Persona[] temp = new Persona[nuevaLongitud];
+
+    for (int i = 0; i < entrada.length; i++) {
+      temp[i] = entrada[i];
+    }
+
+    entrada = temp;
+  }
+
+  private int encuentraIndice(String nombre) {
+    int loc = 0;
+
+    while (loc < contador && nombre.equals(entrada[loc].getNombre())) {
+      loc++;
+    }
+
+    if (loc == contador) {
+      loc = NO_ENCONTRADO;
+    }
+    return loc;
+  }
+}

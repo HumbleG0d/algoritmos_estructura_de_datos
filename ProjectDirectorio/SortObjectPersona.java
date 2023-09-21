74fe11c5474a971a26dbbc7ec600c6c44281db48
HumbleG0d <sergiouni2000@gmail.com>
2023-09-21 13:55:01 -0500

    chamge atribute listaOrdenada to listaPersonas

--- ProjectDirectorio/SortObjectPersona.java
+++ ProjectDirectorio/SortObjectPersona.java
@@ -2,7 +2,7 @@ package ProjectDirectorio;
 
 public class SortObjectPersona {
 
-  public Persona[] burbuja(Persona[] listaOrdenada, int atributo) {
+  public Persona[] burbuja(Persona[] listaPersonas, int atributo) {
 
     Persona p1, p2;
     int resultadoComparacion;
@@ -10,56 +10,56 @@ public class SortObjectPersona {
     int i = 1;
     boolean intercambio = false;
 
-    while (intercambio == false && i < listaOrdenada.length) {
+    while (intercambio == false && i < listaPersonas.length) {
       intercambio = true;
 
-      for (int j = 0; j < listaOrdenada.length - i; j++) {
-        p1 = listaOrdenada[j];
-        p2 = listaOrdenada[j + 1];
+      for (int j = 0; j < listaPersonas.length - i; j++) {
+        p1 = listaPersonas[j];
+        p2 = listaPersonas[j + 1];
         resultadoComparacion = p1.comparaA(p2, atributo);
 
         if (resultadoComparacion > 0) {
-          listaOrdenada[j] = p2;
-          listaOrdenada[j + 1] = p1;
+          listaPersonas[j] = p2;
+          listaPersonas[j + 1] = p1;
           intercambio = false;
         }
       }
       i++;
     }
-    return listaOrdenada;
+    return listaPersonas;
   }
 
-  public void quickSort(Persona[] listaOrdenada, int atributo, int min, int max) {
+  public void quickSort(Persona[] listaPersonas, int atributo, int min, int max) {
     if (min < max) {
-      int indexPivote = partition(listaOrdenada, atributo, min, max);
-      quickSort(listaOrdenada, atributo, min, indexPivote - 1);
-      quickSort(listaOrdenada, atributo, indexPivote + 1, max);
+      int indexPivote = partition(listaPersonas, atributo, min, max);
+      quickSort(listaPersonas, atributo, min, indexPivote - 1);
+      quickSort(listaPersonas, atributo, indexPivote + 1, max);
     }
   }
 
-  private int partition(Persona[] listaOrdenada, int atributo, int min, int max) {
-    Persona pivote = listaOrdenada[max];
+  private int partition(Persona[] listaPersonas, int atributo, int min, int max) {
+    Persona pivote = listaPersonas[max];
     Persona p1;
     int resultadoComparacion;
 
     int i = min - 1;
 
     for (int j = min; j <= max - 1; j++) {
-      p1 = listaOrdenada[j];
+      p1 = listaPersonas[j];
       resultadoComparacion = p1.comparaA(pivote, atributo);
       if (resultadoComparacion < 0) {
         i++;
-        swap(listaOrdenada, i, j);
+        swap(listaPersonas, i, j);
       }
     }
-    swap(listaOrdenada, i + 1, max);
+    swap(listaPersonas, i + 1, max);
     return i + 1;
   }
 
-  private void swap(Persona[] listaOrdenada, int i, int j) {
-    Persona aux = listaOrdenada[i];
-    listaOrdenada[i] = listaOrdenada[j];
-    listaOrdenada[j] = aux;
+  private void swap(Persona[] listaPersonas, int i, int j) {
+    Persona aux = listaPersonas[i];
+    listaPersonas[i] = listaPersonas[j];
+    listaPersonas[j] = aux;
   }
 
   public void mergeSort(Persona[] listaPersonas, int atributo, int min, int max) {
@@ -72,7 +72,7 @@ public class SortObjectPersona {
     }
   }
 
-  private void merge(Persona[] lisPersonas, int atributo, int min, int max, int midle) {
+  private void merge(Persona[] listaPersonas, int atributo, int min, int max, int midle) {
 
     int SIZE1 = midle - min + 1;
     int SIZE2 = max - midle;
@@ -81,10 +81,10 @@ public class SortObjectPersona {
     Persona heighHalf[] = new Persona[SIZE2];
 
     for (int i = 0; i < SIZE1; i++) {
-      lowHalf[i] = lisPersonas[min + i];
+      lowHalf[i] = listaPersonas[min + i];
     }
     for (int i = 0; i < SIZE2; i++) {
-      heighHalf[i] = lisPersonas[midle + 1 + i];
+      heighHalf[i] = listaPersonas[midle + 1 + i];
     }
 
     int i = 0, j = 0, k = min;
@@ -97,22 +97,22 @@ public class SortObjectPersona {
 
       resultadoComparacion = p1.comparaA(p2, atributo);
       if (resultadoComparacion < 0) {
-        lisPersonas[k] = p1;
+        listaPersonas[k] = p1;
         i++;
       } else {
-        lisPersonas[k] = p2;
+        listaPersonas[k] = p2;
         j++;
       }
       k++;
     }
 
     while (i < SIZE1) {
-      lisPersonas[k] = lowHalf[i];
+      listaPersonas[k] = lowHalf[i];
       i++;
       k++;
     }
     while (j < SIZE2) {
-      lisPersonas[k] = heighHalf[j];
+      listaPersonas[k] = heighHalf[j];
       j++;
       k++;
     }

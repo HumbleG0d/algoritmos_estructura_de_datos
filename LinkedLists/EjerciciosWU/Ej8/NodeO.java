49bc18abaa5467508221e125fbc8b9d8a4a478b1
HumbleG0d <sergiouni2000@gmail.com>
2023-09-29 15:00:38 -0500

    Add the PNode code

---
+++ LinkedLists/EjerciciosWU/Ej8/NodeO.java
@@ -0,0 +1,15 @@
+package LinkedLists.EjerciciosWU.Ej8;
+
+public class NodeO {
+    Object value;
+    NodeO next;
+
+    public NodeO(Object value) {
+        this.value = value;
+    }
+
+    public NodeO(Object value, NodeO next) {
+        this.value = value;
+        this.next = next;
+    }
+}

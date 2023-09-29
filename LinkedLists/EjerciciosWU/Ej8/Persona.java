38fe33fd3f36189dae76641cfabb514df9eb1103
HumbleG0d <sergiouni2000@gmail.com>
2023-09-29 15:01:10 -0500

    Add the class Persona code

---
+++ LinkedLists/EjerciciosWU/Ej8/Persona.java
@@ -0,0 +1,26 @@
+package LinkedLists.EjerciciosWU.Ej8;
+
+public class Persona {
+  private String nombre;
+  private int edad;
+
+  public Persona(String nombre) {
+    setNombre(nombre);
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+}

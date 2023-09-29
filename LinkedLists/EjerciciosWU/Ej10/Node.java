6453fb5d2714a40828e61f3f56ab77e7f8ca5c0e
HumbleG0d <sergiouni2000@gmail.com>
2023-09-29 18:12:07 -0500

    Add code Node list

---
+++ LinkedLists/EjerciciosWU/Ej10/Node.java
@@ -0,0 +1,16 @@
+package LinkedLists.EjerciciosWU.Ej10;
+
+public class Node {
+  Object value;
+  Node next;
+
+  public Node(Object value) {
+    this.value = value;
+  }
+
+  public Node(Object value, Node next) {
+    this.value = value;
+    this.next = next;
+  }
+
+}

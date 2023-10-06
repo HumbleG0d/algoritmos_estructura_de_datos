75620725270638e9b529d6c2536d1bcc3fa2408d
HumbleG0d <sergiouni2000@gmail.com>
2023-10-06 16:00:25 -0500

    Change the structure of the heapsort algorithm

--- LinkedLists/Parcial/HeapSort.java
+++ LinkedLists/Parcial/HeapSort.java
@@ -13,93 +13,49 @@ public class HeapSort {
   }
 
   public static void heapSort(DoubleLinkedList doubleList) {
-    DoubleLinkedList newList = new DoubleLinkedList();
-    HeapSortConstruct(doubleList);
-    newList = HeapSortExtract(doubleList);
-    System.out.println(newList.toString());
+    HeapSortExtract(doubleList);
+    System.out.println(doubleList.toString());
   }
 
-  public static void HeapSortConstruct(DoubleLinkedList doubleList) {
-    int current = 0, maxChildIndex;
-    boolean hecho;
-
-    for (int i = (doubleList.getSize() - 2) / 2; i >= 0; i--) {
-      current = i;
-      hecho = false;
-
-      while (!hecho) {
-        if (2 * current + 1 > doubleList.getSize() - 1) {
-          hecho = true;
-        } else {
-          maxChildIndex = HeapSortMaxChild(doubleList, current, doubleList.getSize() - 1);
-          // array[current] < array[maxChildIndex]
-          if (doubleList.find(current) != null || doubleList.find(maxChildIndex) != null) {
-            if (doubleList.find(current).data < doubleList.find(maxChildIndex).data) {
-              intercambio(doubleList, current, maxChildIndex);
-              current = maxChildIndex;
-            } else {
-              hecho = true;
-            }
-          }
-
-        }
-      }
+  public static void HeapSortConstruct(DoubleLinkedList doubleList, int length) {
+    Nodo start = doubleList.head;
+    for (int i = length / 2 - 1; i >= 0; i--) {
+      HeapSortMaxChild(doubleList, length, start);
+      start = start.next;
     }
   }
 
-  public static int HeapSortMaxChild(DoubleLinkedList doubleList, int loc, int end) {
-    int result, Izq, Der;
-    Izq = 2 * loc + 1;
-    Der = 2 * loc + 2;
+  public static void HeapSortMaxChild(DoubleLinkedList doubleList, int length, Nodo current) {
+    Nodo largest = current;
+    Nodo left = current.next;
+    Nodo right = (left != null) ? left.next : null;
 
-    // Check if left child index is within bounds and if the left child exists
-    if (Izq <= end && doubleList.find(Izq) != null) {
-      result = Izq;
+    if (left != null && left.data < largest.data)
+      largest = left;
+    if (right != null && right.data < largest.data)
+      largest = right;
 
-      // Check if right child index is within bounds and if the right child exists
-      if (Der <= end && doubleList.find(Der) != null) {
-        // Compare left and right child data, update result accordingly
-        if (doubleList.find(Izq).data < doubleList.find(Der).data) {
-          result = Der;
-        }
-      }
-    } else {
-      // If left child index is out of bounds or left child doesn't exist, set result
-      // to -1
-      result = -1;
+    if (largest != current) {
+      int temp = largest.data;
+      largest.data = current.data;
+      current.data = temp;
+      HeapSortMaxChild(doubleList, length, current);
     }
-
-    return result;
   }
 
-  public static DoubleLinkedList HeapSortExtract(DoubleLinkedList doubleList) {
-    // int[] Y = new int[array.length];
-    DoubleLinkedList newList = new DoubleLinkedList();
-    int current, maxChildIndex;
-    boolean hecho;
+  public static void HeapSortExtract(DoubleLinkedList doubleList) {
+    int length = doubleList.getSize();
+    HeapSortConstruct(doubleList, length);
 
-    for (int i = doubleList.getSize() - 1; i >= 0; i--) {
-      // Y[i] = array[0];
-      newList.find(i).data = doubleList.find(0).data;
-      // array[0] = array[i];
-      doubleList.find(0).data = doubleList.find(i).data;
-      current = 0;
-      hecho = false;
+    Nodo end = doubleList.ultimo;
+    for (int i = length - 1; i >= 0; i--) {
+      int temp = doubleList.head.data;
+      doubleList.head.data = end.data;
+      end.data = temp;
 
-      while (!hecho) {
-        if (2 * current + 1 > i) {
-          hecho = true;
-        } else {
-          maxChildIndex = HeapSortMaxChild(doubleList, current, i);
-          if (doubleList.find(current).data < doubleList.find(maxChildIndex).data) {
-            intercambio(doubleList, current, maxChildIndex);
-            current = maxChildIndex;
-          } else {
-            hecho = true;
-          }
-        }
-      }
+      length--;
+      HeapSortMaxChild(doubleList, length, end);
+      end = end.prev;
     }
-    return newList;
   }
 }
